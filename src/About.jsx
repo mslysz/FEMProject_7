1defb1f41462e1ac3d732a23ef9f3aa32b16b2a0
mslysz <macslysz@gmail.com>
2024-04-23 07:17:38 +0100

    updated about component structure

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,11 @@ import lightFurniture from './images/image-about-light.jpg';
 const About = () => {
   return (
     <section className='about'>
-      <img src={darkFurniture} alt='dark-furniture-image' />
+      <img
+        className='about__image'
+        src={darkFurniture}
+        alt='dark-furniture-image'
+      />
       <article className='about__info'>
         <h2 className='about__info-title'>ABOUT OUR FURNITURE</h2>
         <p className='about__info-description'>
@@ -16,7 +20,11 @@ const About = () => {
           dream space.
         </p>
       </article>
-      <img src={lightFurniture} alt='light-furniture-image' />
+      <img
+        className='about__image'
+        src={lightFurniture}
+        alt='light-furniture-image'
+      />
     </section>
   );
 };
